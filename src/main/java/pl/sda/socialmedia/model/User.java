f96525bf1a5d52c4ddac14215db5da0be6df5b78
Borys <boria81@gmail.com>
2022-05-30 22:29:22 +0100

    Add user model and entity

---
+++ src/main/java/pl/sda/socialmedia/model/User.java
@@ -0,0 +1,19 @@
+package pl.sda.socialmedia.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private String username;
+    private String password;
+    private String role;
+    private String email;
+
+}
